ac33ac498b7e1d66ac2f32e1577fbf73be92d811
johnmangam <johnmangam@gmail.com>
2016-09-02 20:32:42 +0530

    SIL Converter integration

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
-//using ICU4NET;
-//using ICU4NETExtension;
+using SilEncConverters40;
+using ECInterfaces;
 
 namespace NonUnicodetoUnicodeTool
 {
@@ -9,6 +9,47 @@ namespace NonUnicodetoUnicodeTool
     {
         static void Main(string[] args)
         {
+            // get an instance of the repository object
+            EncConverters aECs = new EncConverters();
+
+            // call AutoSelect to query the user for the converter to use
+            IEncConverter aEC = aECs.NewEncConverterByImplementationType(EncConverters.strTypeSILtec);
+
+            // add tec file
+            aECs.Add("DV_SHREE<>UNICODE", @"H:\WA\Scripts\Marathi NU - U\Font Study\Untitled.tec", ConvType.Legacy_to_from_Unicode, "DV_ME_80", "UNICODE", ProcessTypeFlags.NonUnicodeEncodingConversion);
+
+            // get the configuration interface for this type
+            IEncConverterConfig aConfigurator = aEC.Configurator;
+
+            // there may be some implementation types that don't provide their 
+            // own user-interface, so always check before using it
+            if (aConfigurator != null)
+            {
+                aEC.Configurator.ConversionType = ConvType.Legacy_to_from_Unicode;
+                aEC.Configurator.ConverterIdentifier = "DV_SHREE<>UNICODE";
+                aEC.Configurator.LeftEncodingID = "DV_ME_80";
+                aEC.Configurator.RightEncodingID = "UNICODE";
+
+                // call its Configure method to do the UI
+                if (aConfigurator.Configure(aECs, "DV_SHREE<>UNICODE", ConvType.Legacy_to_from_Unicode, null, null))
+                {
+
+                    // if we reach here, then the converter must have been 
+                    // configured; though, in fact, it might not have been
+                    // added to the System Repository (i.e. it might just 
+                    // be a temporary converter). You can use the 
+                    // rConfigurator.IsInRepository method to detect if it
+                    // was added or not
+                    // now, call the 'Convert' function to do a conversion
+                    string strIn = "ZàÐmàwÂàà - ¥§ý âkwÞm §ýnà";
+                    string strOut = aEC.Convert(strIn);
+
+                    Console.WriteLine(String.Format("'{1}' became '{0}'", strOut, strIn));
+                }
+            }
+
+            //ECInterfaces.ECAttributes at = new ECAttributes(null, null, AttributeType.Converter);
+
             /********************* 
             // Obtaining font File information - Only observation by debugging as of now
             */
@@ -72,9 +113,9 @@ namespace NonUnicodetoUnicodeTool
 
             /*********************
            // TXT FILE: Convert NonUnicode To Unicode
-           */
-            string demo3SourceFilePath = @"H:\WA\Scripts\Demos\Marathi DV font\shusha.txt";
-            string demo3targetFilePath = @"H:\WA\Scripts\Demos\Marathi DV font\shushaUnicode.txt";
+           
+            string demo3SourceFilePath = @"H:\WA\Scripts\Demos\Marathi DV font\marathi.txt";
+            string demo3targetFilePath = @"H:\WA\Scripts\Demos\Marathi DV font\marathiUnicode.txt";
 
            var setCode31 = CharacterSetCodeUtility.GetCharacterSetCode(demo3SourceFilePath);
            if (NonUnicodeToUnicodeUtility.ConvertNonUnicodeToUnicode(57002, demo3SourceFilePath, demo3targetFilePath))
@@ -89,7 +130,7 @@ namespace NonUnicodetoUnicodeTool
            var setCode32 = CharacterSetCodeUtility.GetCharacterSetCode(demo3targetFilePath);
 
            Console.Read();
-           
+           */
 
             /*********************
            // RTF FILE: Convert NonUnicode To Unicode
