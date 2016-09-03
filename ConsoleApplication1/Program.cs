68f2c24fa1bfa540d46ba0cb872c6c9067b2693b
johnmangam <johnmangam@gmail.com>
2016-09-03 15:24:00 +0530

    Conversion using a tec file

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,53 +9,36 @@ namespace NonUnicodetoUnicodeTool
     {
         static void Main(string[] args)
         {
-            // get an instance of the repository object
+            /********************* 
+            
+            // Get an instance of the repository object
             EncConverters aECs = new EncConverters();
 
-            // call AutoSelect to query the user for the converter to use
-            IEncConverter aEC = aECs.NewEncConverterByImplementationType(EncConverters.strTypeSILtec);
-
-            // add tec file
+            // Add Ticket tec file generated from the map file
             aECs.Add("DV_SHREE<>UNICODE", @"H:\WA\Scripts\Marathi NU - U\Font Study\Untitled.tec", ConvType.Legacy_to_from_Unicode, "DV_ME_80", "UNICODE", ProcessTypeFlags.NonUnicodeEncodingConversion);
+            
+            // Get a reference to the converter
+            IEncConverter conv = aECs.GetMapByName("DV_SHREE<>UNICODE");
 
-            // get the configuration interface for this type
-            IEncConverterConfig aConfigurator = aEC.Configurator;
+            // if we reach here, then the converter must have been 
+            // configured; though, in fact, it might not have been
+            // added to the System Repository (i.e. it might just 
+            // be a temporary converter). You can use the 
+            // rConfigurator.IsInRepository method to detect if it
+            // was added or not
+            // now, call the 'Convert' function to do a conversion
+            string strIn = "ZàÐmàwÂàà - ¥§ý âkwÞm §ýnà";
+            string strOut = conv.Convert(strIn);
+            File.WriteAllText(@"H:\Test.txt", strOut);
+            Console.WriteLine(String.Format("'{1}' became '{0}'", strOut, strIn));
+            Console.Read();
 
-            // there may be some implementation types that don't provide their 
-            // own user-interface, so always check before using it
-            if (aConfigurator != null)
-            {
-                aEC.Configurator.ConversionType = ConvType.Legacy_to_from_Unicode;
-                aEC.Configurator.ConverterIdentifier = "DV_SHREE<>UNICODE";
-                aEC.Configurator.LeftEncodingID = "DV_ME_80";
-                aEC.Configurator.RightEncodingID = "UNICODE";
-
-                // call its Configure method to do the UI
-                if (aConfigurator.Configure(aECs, "DV_SHREE<>UNICODE", ConvType.Legacy_to_from_Unicode, null, null))
-                {
-
-                    // if we reach here, then the converter must have been 
-                    // configured; though, in fact, it might not have been
-                    // added to the System Repository (i.e. it might just 
-                    // be a temporary converter). You can use the 
-                    // rConfigurator.IsInRepository method to detect if it
-                    // was added or not
-                    // now, call the 'Convert' function to do a conversion
-                    string strIn = "ZàÐmàwÂàà - ¥§ý âkwÞm §ýnà";
-                    string strOut = aEC.Convert(strIn);
-
-                    Console.WriteLine(String.Format("'{1}' became '{0}'", strOut, strIn));
-                }
-            }
-
-            //ECInterfaces.ECAttributes at = new ECAttributes(null, null, AttributeType.Converter);
+            /********************* 
 
             /********************* 
             // Obtaining font File information - Only observation by debugging as of now
             */
-            //TtfFontFileInformationUtility.GetFontInformation(@"H:\WA\Scripts\Marathi NU - U\MANGAL\MANGAL.TTF");
-            //TtfFontFileInformationUtility.GetFontInformation(@"H:\WA\Scripts\Marathi NU - U\DV_ME_Shree0708.TTF");
-            //TtfFontFileInformationUtility.GetFontInformation(@"H:\WA\Scripts\Marathi NU - U\Fonts as on 26072016\akshar_0.ttf");
+            //TtfFontFileInformationUtility.GetFontInformation(@"H:\WA\Scripts\Marathi NU - U\Fonts as on 26072016\aksh.ttf");
 
             /*********************
             // Obtain the CharacterSet Code
