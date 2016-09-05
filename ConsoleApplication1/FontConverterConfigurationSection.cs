c4dd4052985d667df4fe575265b8a6b2904a6582
johnmangam <johnmangam@gmail.com>
2016-09-05 17:38:15 +0530

    Added configuration class

---
+++ ConsoleApplication1/FontConverterConfigurationSection.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace NonUnicodetoUnicodeTool
+{
+    public class FontConverterConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("fontConverName")]
+        public string FontConverName
+        {
+            get { return (string)this["fontConverName"]; }
+            set { this["fontConverName"] = value; }
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Configuration;
 
 namespace NonUnicodetoUnicodeTool
 {
@@ -7,34 +8,49 @@ namespace NonUnicodetoUnicodeTool
     {
         static void Main(string[] args)
         {
-           if(args.Length == 3)     //  For Non-unicode to unicode conversion
+            if (IsValidInputArguments(args))
             {
-                if (IsValidInputArguments(args))
-                {
-                    NonUnicodeToUnicodeUtility.Convert(args[0], args[1], args[2]);
-                }
-                else
-                {
-                    Console.WriteLine("Check input file"); // other checks need to be made
-                }
+                NonUnicodeToUnicodeUtility.Convert(args[0], args[1], args[2]);
             }
-           else
+            else
             {
-                // Show help
-                Console.WriteLine("Below is the Syntax expected!");
-                Console.WriteLine("Mapcode  sourceFilePath targetFilePath");
-                Console.WriteLine("Refer to FontConversion.Config for different Mapcodes available");
-                Console.WriteLine("sourceFilePath is the file that's text needs to be converted");
-                Console.WriteLine("targetFilePath is the file that's newly generated. If filename is not given, <old>-Unicode.txt gets created in the given folder path.");
+                PrintHelp();
             }
+        }
+
+        static void PrintHelp()
+        {
+            // Show help
+            Console.WriteLine("Below is the Syntax expected!");
+            Console.WriteLine("fontmapcode  sourceFilePath targetFilePath");
+            Console.WriteLine("parameter mapcode: Refer to FontConversion.Config for different Mapcodes available");
+            Console.WriteLine("parameter sourceFilePath: It is the file that's text needs to be converted");
+            Console.WriteLine("parameter targetFilePath: It is the file that's newly generated. If filename is not given, <old>-Unicode.txt gets created in the given folder path.");
         }
 
         static bool IsValidInputArguments(string[] arguments)
         {
-            // check if mapping code exists
-            // check if source file path exists
-            // check if target folder path exists
-            return true;
+            bool isValid = true;
+
+            //  For Non-unicode to unicode conversion
+            if (arguments.Length == 3)     
+            {
+                // check if mapping code exists
+                // refer config map
+
+                // check if source file path exists
+                if (!File.Exists(arguments[1]))
+                {
+                    isValid = false;
+                }
+
+                // check if target folder path exists
+                if (!(Directory.Exists(arguments[2]) || File.Exists(arguments[2])))
+                {
+                    isValid = false;
+                }
+            }
+            return isValid;
         }
     }
 }
